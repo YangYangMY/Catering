4c6a80471df67f354488d779cc9bbf2c8601c765
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-26 19:44:22 +0800

    add adt folder

---
+++ Catering/src/main/java/rsf2s1g2/adt/Queue.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rsf2s1g2.adt;
+
+/**
+ *
+ * @author Wee Yan
+ */
+public class Queue {
+    
+}
