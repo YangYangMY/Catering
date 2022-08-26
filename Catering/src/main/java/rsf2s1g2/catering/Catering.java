cab6321fe271513d6d1e9a1ea70a6b9153ce35d0
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-26 19:35:13 +0800

    apache file

---
+++ Catering/src/main/java/rsf2s1g2/catering/Catering.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package rsf2s1g2.catering;
+
+/**
+ *
+ * @author Wee Yan
+ */
+public class Catering {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
