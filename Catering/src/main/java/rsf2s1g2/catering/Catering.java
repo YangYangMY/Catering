b9c64f464a15a4ca14a51074c5fdecfecd2c4ee9
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-27 16:30:35 +0800

    Add files via upload

--- Catering/src/main/java/rsf2s1g2/catering/Catering.java
+++ Catering/src/main/java/rsf2s1g2/catering/Catering.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package rsf2s1g2.catering;
+
+import java.util.Scanner;
 
 /**
  *
@@ -12,6 +9,33 @@ package rsf2s1g2.catering;
 public class Catering {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        
+
+        int choice = 0;
+        
+        do {
+            try {
+                choice = 0;
+                displayMenu();
+                choice = input.nextInt();
+                if (choice < 1 || choice > 2) {
+                    Screen.clear();
+                    Font.print(Font.ANSI_RED, "                            Only (1-2) is allowed, please try again!\n");
+                }
+
+            } catch (Exception e) {
+                Screen.clear();
+                Font.print(Font.ANSI_RED, "                            Only (1-2) is allowed, please try again!\n");
+                input.next();
+                }
+            }while(choice != 3);
+        }
+        
+    public static void displayMenu(){
+        System.out.println("Catering System");
+        System.out.println("1. Order\n2. Admin\n3. Exit");
+        System.out.println("\nSelect your Choice: ");
     }
+    
 }
---
+++ Catering/src/main/java/rsf2s1g2/catering/Screen.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rsf2s1g2.catering;
+
+/**
+ *
+ * 
+ */
+public class Screen {
+  public static void clear() {
+    try {
+      String operatingSystem = System.getProperty("os.name"); // Check the current operating system
+
+      if (operatingSystem.contains("Windows")) {
+        ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+        Process startProcess = pb.inheritIO().start();
+        startProcess.waitFor();
+      } else {
+        ProcessBuilder pb = new ProcessBuilder("clear");
+        Process startProcess = pb.inheritIO().start();
+
+        startProcess.waitFor();
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+  public static void pause(float seconds) {
+    int miliSeconds = (int)(seconds * 1000);
+    try {
+      Thread.sleep(miliSeconds);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
