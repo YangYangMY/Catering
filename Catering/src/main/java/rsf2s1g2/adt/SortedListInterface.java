c737a5ab68adf1883223e10dc56efbdb2903b6fd
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-29 21:42:59 +0800

    Added Sorted Array List

---
+++ Catering/src/main/java/rsf2s1g2/adt/SortedListInterface.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rsf2s1g2.adt;
+
+/**
+ *
+ * @author Wee Yan
+ * @param <T>
+ */
+public interface SortedListInterface<T extends Comparable<T>> {
+
+  /**
+   * Task: Adds a new entry to the sorted list in its proper order.
+   *
+   * @param newEntry the object to be added as a new entry
+   * @return true if the addition is successful
+   */
+  public boolean add(T newEntry);
+
+  /**
+   * Task: Removes a specified entry from the sorted list.
+   *
+   * @param anEntry the object to be removed
+   * @return true if anEntry was located and removed
+   */
+  public boolean remove(T anEntry);
+
+  public boolean contains(T anEntry);
+
+  public void clear();
+
+  public int getCount();
+
+  public boolean isEmpty();
+
+}
