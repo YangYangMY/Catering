61f1c0354df6619269a7878ab8fc9d68188ee2d4
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-30 23:31:07 +0800

    Update Order.java

--- Catering/src/main/java/rsf2s1g2/Entity/Order.java
+++ Catering/src/main/java/rsf2s1g2/Entity/Order.java
@@ -23,12 +23,7 @@ public class Order implements Comparable<Order> {
     SortedListInterface<Order> AcceptedOrder = new SortedList<>();
     SortedListInterface<Order> RejectedOrder = new SortedList<>();
 
-    public Order(int orderID, String customerName, String date, String phoneNum, double orderAmount){
-        this.orderId = orderID;
-        this.customerName = customerName;
-        this.phoneNum = phoneNum;
-        this.date = date;
-        this.orderAmount = orderAmount;
+    public Order(){
         
     }
     
@@ -40,13 +35,10 @@ public class Order implements Comparable<Order> {
         this.orderAmount = orderAmount;
     }
     
-    public void addOrder(String customerName, String phoneNum, String date, double orderAmount){
+    public void addOrder(String customerName, String date,String phoneNum, double orderAmount){
         orderQueue.enqueue(new Order(customerName, date, phoneNum, orderAmount));
     }
     
-    public void addOrder(int OrderID, String customerName, String phoneNum, String date, double orderAmount){
-        orderQueue.enqueue(new Order(OrderID, customerName, phoneNum, date, orderAmount));
-    }
     
     public void setOrderQueue(CircularQueueInterface<Order> orderQueue){
         this.orderQueue = orderQueue;
@@ -55,12 +47,124 @@ public class Order implements Comparable<Order> {
     public CircularQueueInterface<Order> getOrderQueue(){
         return orderQueue;
     }
+    public void setAcceptedList (SortedListInterface<Order> AcceptedList){
+        this.AcceptedOrder = AcceptedList;
+    }
     
+        public void setRejectedList (SortedListInterface<Order> RejectedList){
+        this.RejectedOrder = RejectedList;
+    }
     
+    public SortedListInterface<Order> getAcceptedList(){
+        return AcceptedOrder;
+    }
     
+    public SortedListInterface<Order> getRejectedList(){
+        return RejectedOrder;
+    }
+
+    public void AcceptOrder(String customerName,String date, String phoneNum , double orderAmount){
+        AcceptedOrder.insert(new Order(customerName,date, phoneNum,  orderAmount));
+    }
+    
+    public void AcceptOrder(Order newEntry){
+        AcceptedOrder.insert(newEntry);
+        orderQueue.dequeue();
+    }
+    
+    public void RejectOrder(Order newEntry){
+        RejectedOrder.insert(newEntry);
+        orderQueue.dequeue();
+    }
+    
+    public void RejectOrder(String customerName,String date, String phoneNum, double orderAmount){
+        RejectedOrder.insert(new Order(customerName,date, phoneNum , orderAmount));
+        orderQueue.dequeue();
+    }
+
+        @Override
+    public String toString() {
+        
+        return date + "\t"+ orderId + "\t   "+"\t\t" + String.format("%.2f",orderAmount) + "\t\t" + customerName+"\t\t" + phoneNum +  "\t\t" + count++;
+                
+              
+    }
     @Override
     public int compareTo(Order o) {
+        return this.date.compareTo(o.getDate());
         
+    }
+
+    public static int getNextOrderID() {
+        return nextOrderID;
+    }
+
+    public static void setNextOrderID(int nextOrderID) {
+        Order.nextOrderID = nextOrderID;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Order.count = count;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(double orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public SortedListInterface<Order> getAcceptedOrder() {
+        return AcceptedOrder;
+    }
+
+    public void setAcceptedOrder(SortedListInterface<Order> acceptedOrder) {
+        AcceptedOrder = acceptedOrder;
+    }
+
+    public SortedListInterface<Order> getRejectedOrder() {
+        return RejectedOrder;
+    }
+
+    public void setRejectedOrder(SortedListInterface<Order> rejectedOrder) {
+        RejectedOrder = rejectedOrder;
     }
    
    
