23eb92eaa4d6c585473c5e5bd035cc02ecf527ff
tgmen00 <75972896+tgmen00@users.noreply.github.com>
2022-08-30 12:36:01 +0800

    Add Map ADT
    Add Map & MapInterface

---
+++ Catering/src/main/java/rsf2s1g2/adt/Map.java
@@ -0,0 +1,106 @@
+package rsf2s1g2.adt;
+
+public class Map<K, V> implements MapInterface <K, V> {
+    
+    private MapTable<K, V>[] hashTable;		
+    private int numOfEntries;
+    private static final int SIZE = 100;
+
+    public Map(){
+        this(SIZE);
+    }
+
+    public Map(int tableSize) {
+        hashTable = new MapTable[tableSize];
+        numOfEntries = 0;
+    }
+
+    @Override
+    public void add(K key, V value) {
+        hashTable[numOfEntries] = new MapTable<K, V>(key, value);
+        numOfEntries++;
+    }
+
+    @Override
+    public void remove(K key) {
+        V valueToRemove = null;
+        
+        int index = getValueIndex(key);
+
+        if(index != -1){
+            valueToRemove = hashTable[index].getValue();
+            hashTable[index].toRemove();
+            numOfEntries--;
+        }
+    }
+
+    @Override
+    public int getValueIndex(K key) {
+        int index = 0;
+
+        for(int i = 0; i < hashTable.length; i++){
+            if(key == hashTable[i].getKey()){
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    @Override
+    public V getValue(K key) {
+        V dataValue = null;
+
+        int index = getValueIndex(key);
+
+        if(index != -1){
+            dataValue = hashTable[index].getValue();
+        }
+
+        return dataValue;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numOfEntries == 0;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getValue(key) != null;
+    }
+
+    private class MapTable<K,V>{
+        private K key;
+        private V value;
+        private boolean inTable;
+        
+        private MapTable(K uniqueKey, V dataValue){
+            key = uniqueKey;
+            value = dataValue;
+            inTable = true;
+        }
+
+        private K getKey(){
+            return key;
+        }
+
+        private V getValue(){
+            return value;
+        }
+
+        private void setValue(V newValue){
+            value = newValue;
+        }
+
+        private boolean isExist(){
+            return inTable;
+        }
+
+        private void toRemove(){
+            key = null;
+            value = null;
+            inTable = false;
+        }
+
+    }
+}
---
+++ Catering/src/main/java/rsf2s1g2/adt/MapInterface.java
@@ -0,0 +1,17 @@
+package rsf2s1g2.adt;
+
+public interface MapInterface<K, V> {
+    
+    public void add(K key, V value);
+
+    public void remove(K key);
+
+    public int getValueIndex(K key);
+
+    public V getValue(K key);
+
+    public boolean isEmpty();
+
+    public boolean contains(K key);
+
+}
