356f9b4b0035bf6852a1a028540ea35948c56dc0
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-08-31 11:16:36 +0800

    Added Login function

--- Catering/src/main/java/Catering.java
+++ Catering/src/main/java/Catering.java
@@ -31,9 +31,9 @@ public class Catering {
 
         //Start Of Program
         int choice = 0;
-        
+        Screen.clear();
         do {
-            Screen.clear();
+            
             try {
                 choice = 0;
                 displayMenu();
@@ -87,9 +87,9 @@ public class Catering {
      }
 
      public static void displayAdminMenu(){
-        System.out.println("\t\t\t\t\tCatering System");
+        System.out.println("\t\t\t\t\tAdmin Panel");
         System.out.println("==================================================================================================");
-        System.out.println("1. Order Evaluation\n2. Accepted Orders\n3. Rejected Orders\n4.Back");
+        System.out.println("1. Order Evaluation\n2. Accepted Orders\n3. Rejected Orders\n4. Back");
         System.out.println("==================================================================================================");
         System.out.print("\nSelect your Choice: ");
     }  
@@ -97,6 +97,54 @@ public class Catering {
 
      public static void Admin(){
         Scanner input1 = new Scanner(System.in);
+        String username = "admin";
+        String password = "123123";
+        int times = 3;
+        boolean contactManagement = true;
+        //Login 
+        System.out.println("\t\t\t\t\tAdmin");
+        System.out.println("==================================================================================================");
+        Font.print(Font.ANSI_BLUE,"                                     Login Page");
+        do {
+            try {
+                System.out.print("                                   Username: ");
+                String enteredUsername = input1.nextLine();
+                System.out.print("                                   Password: ");
+                String enteredPassword = input1.nextLine();
+                if (!enteredUsername.equals(username) || !enteredPassword.equals(password)) {
+                    throw new InvalidException();
+                } else {
+                    //Stop Loop when correct username and password
+                    times = 0;
+                    contactManagement = false;
+
+                    //Enter menu with all objects made
+                    Screen.clear();
+                }
+
+            } catch (InvalidException e) {
+                Screen.clear();
+                System.out.println("\t\t\t\t\tAdmin");
+                System.out.println("==================================================================================================");
+                Font.print(Font.ANSI_BLUE,"                                     Login Page");
+                Font.print(Font.ANSI_RED, e.getErrorMsg());
+                times--;
+                contactManagement = true;
+            }
+        } while (times > 0);
+
+        if (contactManagement) {
+            Screen.clear();
+            System.out.println("\n");
+            Font.print(Font.ANSI_RED, "   Sorry, you have reached the maximum times of login. Please contact the management for the username and password\n");
+            Font.print(Font.ANSI_RED, "   Bringing you back to Home screen in 5 Seconds.\n");
+            Screen.pause(5);
+            Screen.clear();
+            return;
+        }
+    
+
+        
         int choice1 = 0;
         Screen.clear();
         do {
@@ -128,12 +176,28 @@ public class Catering {
                         Screen.clear();
                         
                         break;
-                }
+                    case 4:
+                        Screen.clear();
+                        break;
+                    }
 
             }while(choice1 != 4);
+            Screen.clear();
      }
 
+    // Exception for handling invalid username or password
+    private static class InvalidException extends Exception {
 
+        private final String errorMsg;
+
+        InvalidException() {
+            this.errorMsg = "                     Invalid Password or Username, please try again!\n";
+        }
+
+        public String getErrorMsg() {
+            return errorMsg;
+        }
+    }
 }
 
     
