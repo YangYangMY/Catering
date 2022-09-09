a7139c703da95e87a3c1b8976c86ab996b19868d
tgmen00 <75972896+tgmen00@users.noreply.github.com>
2022-09-09 15:17:41 +0800

    Add customer entity & client

---
+++ Catering/src/main/java/rsf2s1g2/entity/Customer.java
@@ -0,0 +1,55 @@
+package rsf2s1g2.entity;
+
+
+public class Customer{
+    private String name;
+    private String address;
+    private String phoneNum;
+    private String icNo;
+
+    public Customer(){}
+
+    public Customer(String custIcNo, String custName, String custPhoneNum, String custAddress){
+        this.icNo = custIcNo;
+        this.name = custName;
+        this.phoneNum = custPhoneNum;
+        this.address = custAddress;
+    }
+
+    //Getter and Setter
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNum() {
+        return this.phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public void setIcNo(String icNo) {
+        this.icNo = icNo;
+    }
+
+    @Override
+    public String toString() {
+            return "\nName: " + name + 
+            "\nIC No: " + icNo + 
+            "\nPhone No: " + phoneNum + 
+            "\nAddress: " + address + "\n";
+    }
+}
