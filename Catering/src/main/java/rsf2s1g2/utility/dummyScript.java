0a2ee879f1cfa517257bf8a55391c974c87508a7
Lee Wee Yan <69131215+SuMMeRNeVeRLoSe@users.noreply.github.com>
2022-09-09 16:34:23 +0800

    Order dummyscript

--- Catering/src/main/java/rsf2s1g2/utility/dummyScript.java
+++ Catering/src/main/java/rsf2s1g2/utility/dummyScript.java
@@ -4,4 +4,16 @@ import rsf2s1g2.adt.*;
 import rsf2s1g2.entity.*;
 
 public class dummyScript {
+
+    public static Order DummyOrderData(){
+        Order O = new Order();
+        O.addOrder("Lee Wee Yan", "2021-7-12", "0177255766", 15.12);
+        O.addOrder("Cheong Tzen Yang", "2022-5-1", "0177255766", 10003.12);
+        O.addOrder("Tam Ga Men", "2020-1-1", "0177255766", 20.12);
+        O.addOrder("BC", "2019-4-1", "0177255766", 20.12);
+        O.addOrder("Tan Cherng Ming", "2012-8-1", "0175125766", 1020.12);
+        
+        return O;
+    }
+
 }
