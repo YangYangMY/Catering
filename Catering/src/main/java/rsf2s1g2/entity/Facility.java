c408c3e3212ba260d81762476780edf54960c7c6
DuBronC <cheongty-wp20@student.tarc.edu.my>
2022-09-10 16:21:34 +0800

    facility thingsss

---
+++ Catering/src/main/java/rsf2s1g2/entity/Facility.java
@@ -0,0 +1,75 @@
+package rsf2s1g2.entity;
+
+import java.text.DecimalFormat;
+
+public class Facility {
+    private String facilityName;
+    private int facilityNum;
+    private double facilityPrice;
+    private double facilityTotal;
+    private boolean prntString = false;
+    private static double facilityFinalTotal;
+    private static int num = 0;
+    DecimalFormat df = new DecimalFormat("0.00");
+
+    public Facility(String facilityName, double facilityPrice) {
+        this.facilityName = facilityName;
+        this.facilityPrice = facilityPrice;
+        prntString = true;
+    }
+
+    public Facility(String facilityName, int facilityNum, double facilityPrice) {
+        this.facilityName = facilityName;
+        this.facilityNum = facilityNum;
+        this.facilityPrice = facilityPrice;
+        getFacilitytotal();
+    }
+
+    public String getFacilityname() {
+        return facilityName;
+    }
+
+    public void setFacilityname(String facilityName) {
+        this.facilityName = facilityName;
+    }
+
+    public int getFacilitynum() {
+        return facilityNum;
+    }
+
+    public void setFacilitynum(int facilityNum) {
+        this.facilityNum = facilityNum;
+    }
+
+    public double getFacilityprice() {
+        return facilityPrice;
+    }
+    
+    public void setFacilityprice(double facilityPrice) {
+        this.facilityPrice = facilityPrice;
+    }
+
+    public double getFacilitytotal() {
+        facilityTotal = facilityNum * facilityPrice;
+        return facilityTotal;
+    }
+
+    public double getFacilityFinaltotal() {
+        return facilityFinalTotal;
+    }
+
+    public static void resetNumber() {
+        num = 0;
+    }
+
+    @Override
+    public String toString() {
+        if (prntString) {
+            return facilityName + " - " + df.format(facilityPrice);
+        } else {
+            num++;
+            facilityFinalTotal += facilityTotal;
+            return num + ". " + facilityName + " - " + facilityNum + " - " + df.format(facilityPrice) + " - " + df.format(facilityTotal);
+        }
+    }
+}
