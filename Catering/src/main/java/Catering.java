4b560db87b4957c406c832cdf6e277c3adb117d7
Wilson Tan <tancm-wp20@student.tarc.edu.my>
2022-09-16 13:25:34 +0800

    Main program code modify

--- Catering/src/main/java/Catering.java
+++ Catering/src/main/java/Catering.java
@@ -6,7 +6,7 @@ import rsf2s1g2.client.*;
 import rsf2s1g2.entity.*;
 
 public class Catering {
-   
+
     public static void main(String[] args) {
         ListInterface<FoodBeverage> foodlist = new List<FoodBeverage>(10);
         ListInterface<FoodBeverage> beveragelist = new List<FoodBeverage>(10);
@@ -20,185 +20,110 @@ public class Catering {
         ListInterface<Facility> colorselect = new List<Facility>(100);
         ListInterface<Facility> occasionselect = new List<Facility>(100);
         Scanner input = new Scanner(System.in);
-        
-        //Call dummy data from dummyscript
+
+        // Call dummy data from dummyscript
         Order O = dummyScript.DummyOrderData();
 
-        //Start Of Program
-        int choice = 0;
+        // Start Of Program
+        int choice = 0, choice1 = 0;
+        ;
         Screen.clear();
         do {
-            try {
-                choice = 0;
-                displayMenu();
-                choice = input.nextInt();
-                if (choice < 1 || choice > 3) {
-                    Screen.clear();
-                    Font.print(Font.ANSI_RED, "                            Only (1-3) is allowed, please try again!\n");
-                }
-
-            } catch (Exception e) {
-                Screen.clear();
-                Font.print(Font.ANSI_RED, "                            Only (1-3) is allowed, please try again!\n");
-                input.next();
-                }
-                switch(choice){
-                    case 1:
+            displayMenu();
+            choice = Screen.numInputValid(1, 3, "\t\t\t       Select your Choice: ","                            Only (1-3) is allowed, please try again!");
+            Screen.clear();
+            switch (choice) {
+                case 1:
+                    do {
+                        OrderMenu();
+                        choice1 = Screen.numInputValid(1, 4, "\t\t\t       Select your Choice: ", "                            Only (1-4) is allowed, please try again!");
                         Screen.clear();
-                        int choice1 = 0;
-                        Screen.clear();
-                        do {
-                            try {
-                                choice1 = 0;
-                                OrderMenu();
-                                choice1 = input.nextInt();
-                                if (choice1 < 1 || choice1 > 3) {
-                                    Screen.clear();
-                                    Font.print(Font.ANSI_RED, "                            Only (1-4) is allowed, please try again!\n");
-                                }
-                
-                            } catch (Exception e) {
-                                Screen.clear();
-                                Font.print(Font.ANSI_RED, "                            Only (1-4) is allowed, please try again!\n");
-                                input.next();
-                                }
-                                switch(choice1){
-                                    case 1:
-                                        Screen.clear();
-                                        //call food and beverage
-                                        foodandbeverage.foodbeverage(foodlist, beveragelist, foodselect, beverageselect);
-                                        break;
-                                    case 2:
-                                        Screen.clear();
-                                        //call facility
-                                        facilities.facilitiess(facilitySizeList, facilityColorList, facilityOccasionList, sizeselect, colorselect, occasionselect);
-                                        break;
-                                    case 3:
-                                        Screen.clear();
-                                        // payment
-                                        break;
-                                    case 4:
-                                        Screen.clear();
-                                        displayEndScreen();
-                                        break;
-                                }
-                        } while(choice1 != 4);
-                        break;
-                        case 2:
-                        Screen.clear();
-                        
-                        Admin(O);
-                        break;
-                    case 3:
-                        Screen.clear();
-                        displayEndScreen();
-                        break;
-                }
-            }while(choice != 3);
-
-
-        int choice1 = 0;
-        Screen.clear();
-        do {
-            try {
-                choice1 = 0;
-                OrderMenu();
-                choice1 = input.nextInt();
-                if (choice1 < 1 || choice1 > 3) {
-                    Screen.clear();
-                    Font.print(Font.ANSI_RED, "                            Only (1-4) is allowed, please try again!\n");
-                }
-
-            } catch (Exception e) {
-                Screen.clear();
-                Font.print(Font.ANSI_RED, "                            Only (1-4) is allowed, please try again!\n");
-                input.next();
-                }
-                switch(choice1){
-                    case 1:
-                        Screen.clear();
-                        //call food and beverage
-                        foodandbeverage.foodbeverage(foodlist, beveragelist, foodselect, beverageselect);
-                        break;
-                    case 2:
-                        Screen.clear();
-                        //call facility
-                        facilities.facilitiess(facilitySizeList, facilityColorList, facilityOccasionList, sizeselect, colorselect, occasionselect);
-                        break;
-                    case 3:
-                        Screen.clear();
-                        // payment
-                        break;
-                    case 4:
-                        Screen.clear();
-                        displayEndScreen();
-                        break;
-                }
-        } while(choice1 != 4);
+                        switch (choice1) {
+                            case 1: // call food and beverage
+                                foodandbeverage.foodbeverage(foodlist, beveragelist, foodselect, beverageselect);
+                                break;
+                            case 2: // call facility
+                                facilities.facilitiess(facilitySizeList, facilityColorList, facilityOccasionList, sizeselect, colorselect, occasionselect);
+                                break;
+                            case 3: // call payment
+                                break;
+                            case 4:
+                                displayEndScreen();
+                                break;
+                        }
+                    } while (choice1 != 4);
+                    break;
+                case 2:
+                    Admin(O);
+                    break;
+                case 3:
+                    displayEndScreen();
+                    break;
+            }
+        } while (choice != 3);
     }
-        
-     public static void displayMenu(){
-        Font.print(Font.ANSI_PURPLE,"\t\t  ██████╗ █████╗ ████████╗███████╗██████╗ ██╗███╗   ██╗ ██████╗ ");
-        Font.print(Font.ANSI_PURPLE,"\t\t ██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║████╗  ██║██╔════╝ ");
-        Font.print(Font.ANSI_CYAN,"\t\t ██║     ███████║   ██║   █████╗  ██████╔╝██║██╔██╗ ██║██║  ███╗        ");
-        Font.print(Font.ANSI_CYAN,"\t\t ██║     ██╔══██║   ██║   ██╔══╝  ██╔══██╗██║██║╚██╗██║██║   ██║");
-        Font.print(Font.ANSI_YELLOW,"\t\t ╚██████╗██║  ██║   ██║   ███████╗██║  ██║██║██║ ╚████║╚██████╔╝");
-        Font.print(Font.ANSI_YELLOW,"\t\t  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
+
+    public static void displayMenu() {
+        Font.print(Font.ANSI_PURPLE, "\t\t  ██████╗ █████╗ ████████╗███████╗██████╗ ██╗███╗   ██╗ ██████╗ ");
+        Font.print(Font.ANSI_PURPLE, "\t\t ██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║████╗  ██║██╔════╝ ");
+        Font.print(Font.ANSI_CYAN, "\t\t ██║     ███████║   ██║   █████╗  ██████╔╝██║██╔██╗ ██║██║  ███╗ ");
+        Font.print(Font.ANSI_CYAN, "\t\t ██║     ██╔══██║   ██║   ██╔══╝  ██╔══██╗██║██║╚██╗██║██║   ██║");
+        Font.print(Font.ANSI_YELLOW, "\t\t ╚██████╗██║  ██║   ██║   ███████╗██║  ██║██║██║ ╚████║╚██████╔╝");
+        Font.print(Font.ANSI_YELLOW, "\t\t  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
         System.out.println("==================================================================================================");
         System.out.println("\t\t\t\t\t1. Order\n\t\t\t\t\t2. Admin\n\t\t\t\t\t3. Exit");
         System.out.println("==================================================================================================");
-        System.out.print("\n\t\t\t       Select your Choice: ");
-    }  
-    
+    }
+
     public static void OrderMenu() {
-        Font.print(Font.ANSI_PURPLE,"\t\t  ██████╗ █████╗ ████████╗███████╗██████╗ ██╗███╗   ██╗ ██████╗ ");
-        Font.print(Font.ANSI_PURPLE,"\t\t ██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║████╗  ██║██╔════╝ ");
-        Font.print(Font.ANSI_CYAN,"\t\t ██║     ███████║   ██║   █████╗  ██████╔╝██║██╔██╗ ██║██║  ███╗        ");
-        Font.print(Font.ANSI_CYAN,"\t\t ██║     ██╔══██║   ██║   ██╔══╝  ██╔══██╗██║██║╚██╗██║██║   ██║");
-        Font.print(Font.ANSI_YELLOW,"\t\t ╚██████╗██║  ██║   ██║   ███████╗██║  ██║██║██║ ╚████║╚██████╔╝");
-        Font.print(Font.ANSI_YELLOW,"\t\t  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
+        Font.print(Font.ANSI_PURPLE, "\t\t  ██████╗ █████╗ ████████╗███████╗██████╗ ██╗███╗   ██╗ ██████╗ ");
+        Font.print(Font.ANSI_PURPLE, "\t\t ██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║████╗  ██║██╔════╝ ");
+        Font.print(Font.ANSI_CYAN, "\t\t ██║     ███████║   ██║   █████╗  ██████╔╝██║██╔██╗ ██║██║  ███╗ ");
+        Font.print(Font.ANSI_CYAN, "\t\t ██║     ██╔══██║   ██║   ██╔══╝  ██╔══██╗██║██║╚██╗██║██║   ██║");
+        Font.print(Font.ANSI_YELLOW, "\t\t ╚██████╗██║  ██║   ██║   ███████╗██║  ██║██║██║ ╚████║╚██████╔╝");
+        Font.print(Font.ANSI_YELLOW, "\t\t  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
         System.out.println("==================================================================================================");
         System.out.println("\t\t\t\t\t1. Food & Beverage\n\t\t\t\t\t2. Facility\n\t\t\t\t\t3. Payment\n\t\t\t\t\t4. Exit");
         System.out.println("==================================================================================================");
-        System.out.print("\n\t\t\t       Select your Choice: ");
     }
-    
-     public static void displayEndScreen(){
+
+    public static void displayEndScreen() {
         Font.print(Font.ANSI_YELLOW,"████████╗██╗  ██╗ █████╗ ███╗   ██╗██╗  ██╗    ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗██╗");
         Font.print(Font.ANSI_YELLOW,"╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██║ ██╔╝    ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║██║");
         Font.print(Font.ANSI_YELLOW,"   ██║   ███████║███████║██╔██╗ ██║█████╔╝      ╚████╔╝ ██║   ██║██║   ██║    ██║██║");
         Font.print(Font.ANSI_YELLOW,"   ██║   ██╔══██║██╔══██║██║╚██╗██║██╔═██╗       ╚██╔╝  ██║   ██║██║   ██║    ╚═╝╚═╝");
         Font.print(Font.ANSI_YELLOW,"   ██║   ██║  ██║██║  ██║██║ ╚████║██║  ██╗       ██║   ╚██████╔╝╚██████╔╝    ██╗██╗");
         Font.print(Font.ANSI_YELLOW,"   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝       ╚═╝    ╚═════╝  ╚═════╝     ╚═╝╚═╝");
-     }
+        continueMessage();
+    }
 
+    // START OF ADMIN SECTION
+    public static void Admin(Order O) {
 
-
-    //START OF ADMIN SECTION 
-
-
-
-     public static void Admin(Order O){
-     
         boolean check = admin.Login();
-        if (check == true){
+        if (check == true) {
             admin.Menu(O);
         }
-     }
+    }
 
-     // Exception for handling invalid username or password
-        private static class InvalidException extends Exception {
+    // Exception for handling invalid username or password
+    private static class InvalidException extends Exception {
 
-            private final String errorMsg;
-    
-            InvalidException() {
-                this.errorMsg = "                     Invalid Password or Username, please try again!\n";
-            }
-    
-            public String getErrorMsg() {
-                return errorMsg;
-            }
+        private final String errorMsg;
+
+        InvalidException() {
+            this.errorMsg = "                     Invalid Password or Username, please try again!\n";
         }
-}
 
-    
+        public String getErrorMsg() {
+            return errorMsg;
+        }
+    }
+
+    public static void continueMessage(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("\t\t\t       Enter to continue...");
+        input.nextLine();
+        Screen.clear();
+    }
+}
