cb05b4de64659855a4ddf72ccd0098a57fc74f4d
Wilson Tan <tancm-wp20@student.tarc.edu.my>
2022-09-16 12:32:30 +0800

    int validation function

--- Catering/src/main/java/rsf2s1g2/utility/Screen.java
+++ Catering/src/main/java/rsf2s1g2/utility/Screen.java
@@ -1,5 +1,7 @@
 package rsf2s1g2.utility;
 
+import java.util.Scanner;
+
 /**
  *
  * 
@@ -23,12 +25,30 @@ public class Screen {
       System.out.println(e);
     }
   }
+
   public static void pause(float seconds) {
-    int miliSeconds = (int)(seconds * 1000);
+    int miliSeconds = (int) (seconds * 1000);
     try {
       Thread.sleep(miliSeconds);
     } catch (Exception e) {
       System.out.println(e);
     }
+  }
+
+  public static int numInputValid(int min, int max, String message) {
+    Scanner input = new Scanner(System.in);
+    int num = 0;
+    try {
+      System.out.print(message);
+      num = input.nextInt();
+      if (num < min || num > max) {
+        System.out.println("Invalid input. Please try again.");
+        num = numInputValid(min, max, message);
+      }
+    } catch (Exception e) {
+      System.out.println("Invalid input. Please try again.");
+      num = numInputValid(min, max, message);
+    }
+    return num;
   }
 }
